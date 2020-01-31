83924dc8924612647f7ed60c256eeb2579ff77ca
Karnam <sudhakak@c4b301bec649.ant.amazon.com>
2020-01-31 14:03:40 -0800

    initial commit from Spring init

---
+++ songr/src/main/java/com/sudha/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.sudha.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
